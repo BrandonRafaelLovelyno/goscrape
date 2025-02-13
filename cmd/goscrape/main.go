2866d215d91de8735403401150a0dd57f470fc88
Brandon <brandonrafael80@gmail.com>
2025-02-13 20:09:55 +0700

    fix: committed unresolved merging

--- cmd/goscrape/main.go
+++ cmd/goscrape/main.go
@@ -15,22 +15,6 @@ func main() {
 		log.Fatalf("failed to get command arguments: %v", err)
 	}
 
-<<<<<<< Updated upstream
 	header := scraping.NewScraperHeader("Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36", nil)
-	scraper := scraping.NewScraper(arg.Url, header)
-=======
-	scraper := scraping.NewScraper(arg.Url)
-
-	jsonData, err := scraper.Scrape()
-	if err != nil {
-		log.Fatalf("failed to scrape: %v", err)
-	}
-
-	err = json.WriteToJson(jsonData, arg.OutDir)
-	if err != nil {
-		log.Fatalf("failed to write to json: %v", err)
-	}
-
-	log.Printf("Result exported to: %v", arg.OutDir)
->>>>>>> Stashed changes
+	scraping.NewScraper(arg.Url, header)
 }
